1ee01a016f09ab7330ce014aced263153a9e4817
Mikalai Rusakovich <kolyos83@mail.ru>
2024-03-19 03:10:58 +0300

    refactor: delete comments

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -13,19 +13,4 @@ import {
 export class PlayerComponent {
   @ViewChild('player', {static: false}) playerRef!: ElementRef<HTMLAudioElement>;
   @Input() src = '';
-
-  // For automatic load track
-
-  // reloadAudio() {
-  //   const player = this.playerRef.nativeElement;
-  //   player.pause();
-  //   player.load();
-  //   player.play();
-  // }
-  //
-  // ngAfterViewChecked(): void {
-  //   if (this.src) {
-  //     this.reloadAudio();
-  //   }
-  // }
 }
